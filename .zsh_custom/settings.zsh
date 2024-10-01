6156f9c7c7d59942ea918efecc5fd9a7f9c0aa6c
Jacob Kaplan-Moss <jacob@jacobian.org>
2024-09-30 17:34:34 -0700

    atuin

--- .zsh_custom/settings.zsh
+++ .zsh_custom/settings.zsh
@@ -14,19 +14,15 @@ export TERM=xterm-256color
 unset MAILCHECK
 
 # Fancy term stuff
+
+# ez
 export EZA_CONFIG_DIR=$HOME/.config/eza
+
+# Bat
 export BAT_THEME=Dracula
+
+# atuin
+eval "$(atuin init zsh --disable-up-arrow)"
 
 # Don't expand globs when tab completing
 setopt GLOB_COMPLETE
-
-# History settings
-HISTFILE=${ZDOTDIR:-$HOME}/.zsh_history
-setopt EXTENDED_HISTORY       # include timestamps/etc in history
-setopt APPEND_HISTORY         # append (instead of overwriting) history
-setopt SHARE_HISTORY          # share history between shells
-setopt HIST_EXPIRE_DUPS_FIRST # delete duplicate lines from history first
-setopt HIST_FIND_NO_DUPS      # ignore duplicates when searching
-setopt HIST_REDUCE_BLANKS     # removes blank lines from history
-SAVEHIST=1000000              # save a lot of history
-HISTSIZE=1000000              # save a lot of history
