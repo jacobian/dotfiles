b2082604943551cc2d94208c47e56480f0d3b34a
Jacob Kaplan-Moss <jacob@jacobian.org>
2024-09-30 17:09:52 -0700

    wezterm tabs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,11 +2,15 @@ local wezterm = require 'wezterm';
 local config = wezterm.config_builder()
 
 config.color_scheme = "Dracula"
-config.font = wezterm.font({
-    family = "SauceCodePro NFM",
-})
+config.font = wezterm.font { family = "SauceCodePro NFM" }
 config.font_size = 15
 config.line_height = 1.1
 config.quit_when_all_windows_are_closed = false
+
+config.window_frame = {
+    font = wezterm.font { family = 'SauceCodePro NFM', weight = 'Bold' },
+    font_size = 15,
+  }
+config.window_decorations = 'RESIZE'
 
 return config
